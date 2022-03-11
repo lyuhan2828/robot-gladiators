2507c31893574614a57bb431e50bb158879837bf
Yu Han <yuhan28283@gmail.com>
2022-03-11 01:38:16 -0500

    update

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,7 +8,7 @@ var playerAttack = 10;
 console.log(playerName, playerAttack, playerHealth);
 
 var enemyName = "Roborto";
-var enemyHealth = 50;
+var enemyHealth = 9;
 var enemyAttack = 12;
 
 // create function
@@ -29,7 +29,7 @@ enemyHealth = enemyHealth - playerAttack;
   else {
     window.alert(enemyName + "still has " + enemyHealth + " health left.");
   }
-  
+
  // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable
 playerHealth = playerHealth - enemyAttack;
 
